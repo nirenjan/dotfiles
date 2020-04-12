9da5e26220c1c6a315f1e230309aafb332b090db
nirenjan <nirenjan@gmail.com>
2020-04-12 16:57:52 -0700

    Get SSH_AUTH_SOCK from gpgconf
    Older versions of GPG don't support the `agent-ssh-socket` option, so we
    will have to work around this.

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -1,3 +1,10 @@
 gpgconf --launch gpg-agent
 
 export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+
+TSOCK=$(gpgconf --list-dirs agent-ssh-socket || true)
+if [[ -n "$TSOCK" ]]
+then
+    export SSH_AUTH_SOCK="$TSOCK"
+    unset TSOCK
+fi
