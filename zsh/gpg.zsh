a866ab7d4e10b2c2923246d52ff3af61d4c2c6df
nirenjan <nirenjan@gmail.com>
2020-09-09 07:49:26 -0700

    Wrap gpg snippet to ensure that the command is available

--- zsh/gpg.zsh
+++ zsh/gpg.zsh
@@ -1,10 +1,13 @@
-gpgconf --launch gpg-agent
-
-export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
-
-TSOCK=$(gpgconf --list-dirs agent-ssh-socket || true)
-if [[ -n "$TSOCK" ]]
+if command -v gpgconf >/dev/null
 then
-    export SSH_AUTH_SOCK="$TSOCK"
-    unset TSOCK
+    gpgconf --launch gpg-agent
+
+    export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+
+    TSOCK=$(gpgconf --list-dirs agent-ssh-socket || true)
+    if [[ -n "$TSOCK" ]]
+    then
+        export SSH_AUTH_SOCK="$TSOCK"
+        unset TSOCK
+    fi
 fi
