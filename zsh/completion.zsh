45044ab177d530c1e1486d6c487cfdff633f83b1
nirenjan <nirenjan@gmail.com>
2023-06-05 08:55:47 -0700

    Add command_not_found completion to zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -5,3 +5,12 @@ zstyle :compinstall filename '/home/nkrishnan/.zshrc'
 
 autoload -Uz compinit
 compinit
+
+#######################################################################
+# ZSH Command suggestions
+#######################################################################
+# This is only valid on Ubuntu, I haven't found a Mac OS equivalent
+if [[ -e /etc/zsh_command_not_found ]]
+then
+    source /etc/zsh_command_not_found
+fi
